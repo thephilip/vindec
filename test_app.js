2a28b3ec632ba2ede7dfda4d45d7b7da51fc196d
Philip Smith <thephilipsmith@gmail.com>
2018-05-03 12:13:42 -0400

    typo in test_app

--- test_app.js
+++ test_app.js
@@ -3,4 +3,4 @@ const vindec = require('./index.js')
 const vin = '1FTRX12V69FA1124'
 
 console.log(`\n valid: ${vindec.validate(vin)}`)
-console.log(`\n decode: ${vindec.decode(vin)}`)
+console.log(`\n decode: ${JSON.stringify(vindec.decode(vin))}`)

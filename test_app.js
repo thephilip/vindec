1aa6bcf2570b2666ccbfbcea4d233a9ca0f07e4c
Philip Smith <thephilipsmith@gmail.com>
2019-01-09 14:28:10 -0500

    remove junk

--- test_app.js
+++
@@ -1,6 +0,0 @@
-
-const vindec = require('./index.js')
-const vin = '1FTRX12V69FA11242'
-
-console.log(`\n valid: ${vindec.validate(vin)}`)
-console.log(`\n decode: ${JSON.stringify(vindec.decode(vin))}`)
